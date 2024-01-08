1cbf0af672a8b204f51d3c481d00fc453a78dc05
Shivam <sspatel120702@gmail.com>
2024-01-08 15:27:44 +0530

    basic express and mainly work on utils

---
+++ src/utils/ApiError.js
@@ -0,0 +1,25 @@
+// https://nodejs.org/api/errors.html
+
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    stack = "",
+    errors = []
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
